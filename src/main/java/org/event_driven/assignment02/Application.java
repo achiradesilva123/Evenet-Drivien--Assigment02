377ed17eaa5c7492498595fa5b6d93b6cf37cfce
achiradesilva123 <achiradceyentra.com>
2024-10-02 11:56:42 +0930

    created the main class to run the program.

---
+++ src/main/java/org/event_driven/assignment02/Application.java
@@ -0,0 +1,9 @@
+package org.event_driven.assignment02;
+
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Application => {} :Train InterLocking System v-0.0.1");
+    }
+}
